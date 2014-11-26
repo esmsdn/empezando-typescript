51e4e41fd4f2ba7a7825930f753f50214e923004
CKGrafico <quique@ckgrafico.com>
2014-11-26 10:59:20 +0100

    upload example

---
+++ Solveed/app.js
@@ -0,0 +1,145 @@
+﻿(function() {  
+	// variables de configuración 
+	var elapsedSeconds = 0; 
+	var width = 10, height = 10, minesCount = 10;
+
+	window.onload = function() { 
+		setInterval(function() { 
+			elapsedSeconds += 1; 
+			document.getElementById('timer').innerHTML = elapsedSeconds; 
+		}, 1000);
+
+		drawMines(width, height, 'mines'); 
+		matrix = generateField(width, height, minesCount);                  
+		buttons = document.getElementsByName('btn');
+
+		for (var i = 0; i < buttons.length; i++) { 
+			document.getElementById(buttons[i].id).onclick = function(e) { 
+			var point = e.target.id.split('_'); 
+			var value = matrix[parseInt(point[0])][parseInt(point[1])]; 
+
+			if (matrix[parseInt(point[0])][parseInt(point[1])] == '*') {
+				e.target.value = '*'; 
+				alert('You lose!'); 
+				location.href = location.href;
+			} else { 
+				expand(parseInt(point[0]), parseInt(point[1]), matrix);                 
+			} 
+ 
+			if (isWinner(width, height, minesCount)) { 
+				alert('You win!'); 
+				location.href = location.href;
+			} 
+		} 
+	} 
+}})(); 
+ 
+ 
+ 
+function isWinner(width, height, minesNumber) {  
+	var enabledCounter = 0; 
+ 
+	for (var y = 0; y < height; y++) {
+
+		for (var x=0; x<width; x++) { 
+			btn = document.getElementById(x + '_' + y);
+
+			if (!btn.disabled) {
+				enabledCounter++;
+			}
+		} 
+	}    
+	return enabledCounter == minesNumber; 
+} 
+ 
+function expand(x, y, matrix) { 
+	btn = document.getElementById(x + '_' + y); 
+	var value = matrix[x][y]; 
+
+	if (btn.disabled) {
+		return false;
+	}
+
+	if (value && value != '*') { 
+		btn.value = value; 
+		btn.disabled = 'true'; 
+		return false; 
+	} 
+ 
+	if (!value) { 
+		btn.value = ' '; 
+		btn.disabled = 'true'; 
+	} 
+ 
+	var limits = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1], [x - 1, y - 1], [x + 1, y - 1], [x - 1, y + 1], [x + 1, y + 1]];
+
+	for (var i=0; i<limits.length; i++) { 
+		try { 
+ 
+			if (matrix[limits[i][0]][limits[i][1]] != '*') {  
+				expand(limits[i][0], limits[i][1], matrix);  
+			} 
+		} catch(e) { } 
+	} 
+} 
+ 
+function generateField(width, height, maxMines) { 
+	var field = makeMatrix(width, height); 
+	var minesCounter = 0; 
+
+	while (minesCounter < maxMines) {
+		var randomMine = getRandom(0, 1); 
+		var randomPosition = [getRandom(0, width - 1), getRandom(0, height - 1)]; 
+
+		if (!field[randomPosition[0]][randomPosition[1]]) {
+			minesCounter += (randomMine) ? 1 : 0; 
+
+			if (randomMine) {
+				field[randomPosition[0]][randomPosition[1]] =  '*'; 
+
+				for (var x = randomPosition[0] - 1; x <= randomPosition[0] + 1; x++) {
+ 
+					for (var y=randomPosition[1]-1; y<=randomPosition[1]+1; y++) { 
+						try { 
+							field[x][y] += (field[x][y] != '*') ? 1 : ''; 
+						}catch(e) { }//TypeError probablemente 
+					} 
+				} 
+			} 
+		} 
+	} 
+	return field; 
+}
+
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)); 
+} 
+ 
+function makeMatrix(width, height) { 
+	var matrix = new Array(height); 
+ 
+	for (var i=0; i<height; i++) { 
+		matrix[i] = new Array(width); 
+
+		for (var j = 0; j < width; j++) {
+			matrix[i][j] = 0; 
+		} 
+	} 
+	return matrix; 
+} 
+ 
+ 
+ 
+function drawMines(width, height, elementId) { 
+	var div = document.getElementById(elementId); 
+	div.innerHTML = ''; 
+
+	for (var i = 0; i < height; i++) {
+
+		for (var j = 0; j < width; j++) {
+			div.innerHTML  += '<input type="button" name="btn" id="' + i + '_' + j + '" style="width:25px; height:25px" value=" "/>'; 
+		} 
+		div.innerHTML += '<br/>'; 
+	} 
+} 
+ 
---
+++ TypeMinas/app.ts
@@ -0,0 +1,174 @@
+﻿module App {
+    export class Game {
+        private elapsedSeconds: number;
+        private width: number;
+        private height: number; 
+        private minesCount: number;
+        private interval: number;
+        private buttons: any;
+        private matrix: Array<Array<number>>;
+
+        constructor(elapsedSeconds, width, height, minesCount) {
+            this.elapsedSeconds = elapsedSeconds;
+            this.width = width;
+            this.height = height;
+            this.minesCount = minesCount;
+
+            this.initializeTimer();
+
+            this.drawMines('mines');
+            this.generateField();
+            this.buttons = document.getElementsByName('btn');
+
+            this.initializeGame();
+        } 
+
+        initializeTimer() {
+            var self = this;
+            this.interval = setInterval(() => {
+              self.elapsedSeconds++;
+              document.getElementById('timer').innerHTML = self.elapsedSeconds.toString();  
+            }, 1000);
+        }
+
+        drawMines(elementId) {
+            var div = document.getElementById(elementId);
+            div.innerHTML = '';
+            for (var i = 0; i < this.height; i++) {
+                for (var j = 0; j < this.width; j++) {
+                    div.innerHTML += '<input type="button" name="btn" id="' + i + '_' + j + '" style="width:25px; height:25px" value=" "/>';
+                }
+                div.innerHTML += '<br/>';
+            } 
+        }
+
+        generateField() {
+            var field = this.makeMatrix();
+            var minesCounter = 0;
+            var n: number;
+
+            while (minesCounter < this.minesCount) {
+                var randomMine = this.getRandom(0, 1);
+                var randomPosition = [this.getRandom(0, this.width - 1), this.getRandom(0, this.height - 1)];
+
+                if (!field[randomPosition[0]][randomPosition[1]]) {
+                    minesCounter += (randomMine) ? 1 : 0;
+
+                    if (randomMine) {
+                        field[randomPosition[0]][randomPosition[1]] = -10;
+
+                        for (var x = randomPosition[0] - 1; x <= randomPosition[0] + 1; x++) {
+
+                            for (var y = randomPosition[1] - 1; y <= randomPosition[1] + 1; y++) {
+                                try {
+                                    n = +field[x][y];
+                                    if (field[x][y] != -10) {
+                                        n += 1;
+                                        field[x][y] = n;
+                                    }
+                                } catch (e) { }//TypeError probablemente 
+                            }
+                        }
+                    }
+                }
+            }
+            this.matrix = field;
+        }
+
+        private makeMatrix() {
+            var matrix: Array<Array<number>>;
+            matrix = new Array(this.height);
+
+            for (var i = 0; i < this.height; i++) {
+                matrix[i] = new Array(this.width);
+
+                for (var j = 0; j < this.width; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+            return matrix;
+        }
+
+        private getRandom(min, max) {
+            return Math.floor(Math.random() * (max - min + 1));
+        } 
+
+        private initializeGame() {
+            var self = this;
+            for (var i = 0; i < this.buttons.length; i++) {
+                document.getElementById(this.buttons[i].id).onclick = (e) => {
+                    var event: any;
+                    event = e;
+                    var point = event.target.id.split('_');
+                    var value = self.matrix[parseInt(point[0])][parseInt(point[1])];
+
+                    if (self.matrix[parseInt(point[0])][parseInt(point[1])] == -10) {
+                        event.target.value = '*';
+                        alert('You lose!');
+                        location.href = location.href;
+                    } else {
+                        self.expand(parseInt(point[0]), parseInt(point[1]), this.matrix);
+                    }
+
+                    if (self.isWinner(this.width, this.height, this.minesCount)) {
+                        alert('You win!');
+                        location.href = location.href;
+                    }
+                }
+            }
+        }
+
+        isWinner(width, height, minesNumber) {
+            var enabledCounter = 0;
+            var btn: any;
+
+            for (var y = 0; y < height; y++) {
+
+                for (var x = 0; x < width; x++) {
+                    btn = document.getElementById(x + '_' + y);
+
+                    if (!btn.disabled) {
+                        enabledCounter++;
+                    }
+                }
+            }
+            return enabledCounter == minesNumber;
+        }
+
+        private expand(x, y, matrix) {
+            var btn: any;
+            btn = document.getElementById(x + '_' + y);
+            var value = matrix[x][y];
+
+            if (btn.disabled) {
+                return false;
+            }
+
+            if (value && value != '*') {
+                btn.value = value;
+                btn.disabled = 'true';
+                return false;
+            }
+
+            if (!value) {
+                btn.value = ' ';
+                btn.disabled = 'true';
+            }
+
+            var limits = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1], [x - 1, y - 1], [x + 1, y - 1], [x - 1, y + 1], [x + 1, y + 1]];
+
+            for (var i = 0; i < limits.length; i++) {
+                try {
+
+                    if (matrix[limits[i][0]][limits[i][1]] != '*') {
+                        this.expand(limits[i][0], limits[i][1], matrix);
+                    }
+                } catch (e) { }
+            }
+        } 
+    }
+}
+
+window.onload = () => {
+    var game = new App.Game(0, 10, 10, 10);
+};
